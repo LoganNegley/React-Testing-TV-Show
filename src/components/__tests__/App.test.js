ce10c7ffdefe5a496c9c993c9b091fd2f02839ef
Logan Negley <logannegley@gmail.com>
2020-03-11 19:31:33 -0500

    no changes

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,10 +5,5 @@ import App from '../../App';
 
 
 test('Name of show is rendering', () => {
-    const mockGetData = jest.fn();
-    const {getByText} = render(<App getData={mockGetData} isFetchingData={true}/>);
-    
-    getByText(/fetching data.../i)
-    expect(getBuy);
     
 });
