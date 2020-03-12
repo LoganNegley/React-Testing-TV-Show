4199d6dc64bdf3aea89306ef19d5289b008813bf
Logan Negley <logannegley@gmail.com>
2020-03-11 20:27:12 -0500

    episodes test file

--- src/components/__tests__/Episodes.test.js
+++ src/components/__tests__/Episodes.test.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import App from '../../App';
+import Episodes from '../Episodes';
+
+test('', () => {
 
 
-
-test('fetching data is being rendered', () => {
-    const mockGetData = jest.fn();
-    const {getByText} = render(<App getData = {mockGetData} show={false}/>)
-    const isLoading = getByText(/fetching data/i)
-    expect(isLoading).toBeInTheDocument();
-    expect(isLoading).toBeTruthy();
 });
-
-
 
 // test("renders greeting on Greeting component", async () => {
 //   // Arrange
